de557b918f7582e5bdab34a3bb770ecbce50388f
Adam <adamek.og@gmail.com>
2017-07-16 00:13:08 +0200

    + added Food renderer + added Organism renderer

---
+++ Mindvolving.Visualization/Renderers/Entities/FoodEntityRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Mindvolving.Visualization.Engine.Entities;
+
+namespace Mindvolving.Visualization.Renderers.Entities
+{
+    public class FoodEntityRenderer : IRenderable
+    {
+        public MindvolvingVisualization Visualization { get; set; }
+        public Food Food { get; private set; }
+
+        public FoodEntityRenderer(Food food)
+        {
+            Food = food;
+        }
+
+        public void Draw(GameTime gt)
+        {
+            Visualization.Primitive2DRenderer.FillCircle(Food.Position, 20, Color.Green);
+        }
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
