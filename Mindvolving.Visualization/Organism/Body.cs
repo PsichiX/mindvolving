24aa1ff908dcfed483017fa24a31ea344d0ece3d
Adam <adamek.og@gmail.com>
2017-07-15 02:35:07 +0200

    + added missing body parts

--- Mindvolving.Visualization/Organism/Body.cs
+++ Mindvolving.Visualization/Organism/Body.cs
@@ -5,13 +5,21 @@ namespace Mindvolving.Visualization.Organism
     public class Body
     {
         public List<Muscle> Muscles { get; private set; }
-
+        public List<BodyPart> BodyParts { get; private set; }
         public Skeleton Skeleton { get; private set; }
 
         public Body()
         {
+            BodyParts = new List<BodyPart>();
             Skeleton = new Skeleton();
             Muscles = new List<Muscle>();
+        }
+
+        public BodyPart CreateBodyPart()
+        {
+            BodyPart part = new BodyPart(this);
+            BodyParts.Add(part);
+            return part;
         }
 
         public void CreateMuscle(BodyPart part1, BodyPart part2)
