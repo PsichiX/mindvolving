b4f5a1cffdb3d67419cf73499ac9a418a9a77a18
PsichiX <psichix@gmail.com>
2017-07-17 01:30:58 +0200

    physics user data

---
+++ Mindvolving/Organisms/Physics/BoneUserData.cs
@@ -0,0 +1,9 @@
+﻿namespace Mindvolving.Organisms.Physics
+{
+	public class BoneUserData : IPhysicsUserData
+	{
+		public Organ From { get; internal set; }
+		public Organ To { get; internal set; }
+		public object CustomData { get; set; }
+	}
+}
